f08848f2d97731fa2746015cbd870038a5d9a433
sanjay <sanjay@DESKTOP-VGU5P9Q>
2021-04-11 10:17:55 +0530

    [Sanjay]: adding code to new branch

--- src/demo/sample3.java
+++ src/demo/sample3.java
@@ -3,5 +3,10 @@ package demo;
 public class sample3 {
 public static void main(String[] args) {
 	System.out.println("hoe are you");
+	
+	System.out.println("hoe are you");
+	
+	System.out.println("hoe are you");
+	System.out.println("hoe are you");
 }
 }
